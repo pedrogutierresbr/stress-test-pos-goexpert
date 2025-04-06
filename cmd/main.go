e032ef3693a9a3b6f7bab7c2e79f052cdaa2011b
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2025-04-06 20:57:12 -0300

    implement main function for load testing with URL, requests, and concurrency flags

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/pedrogutierresbr/stress-test-pos-goexpert/internal/stress_test"
+)
+
+func main() {
+	url := flag.String("url", "", "URL do serviço a ser testado")
+	requests := flag.Int("requests", 100, "Número total de requests")
+	concurrency := flag.Int("concurrency", 10, "Número de chamadas simultâneas")
+	flag.Parse()
+	if *url == "" {
+		fmt.Println("A URL deve ser fornecida")
+		os.Exit(1)
+	}
+	startTime := time.Now()
+	results := stress_test.RunLoadTest(*url, *requests, *concurrency)
+	duration := time.Since(startTime)
+	stress_test.GenerateReport(results, duration)
+}
